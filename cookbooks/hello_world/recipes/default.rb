a6a1a25d6f50d036f7d2923f17b41e98e0968438
EfrainOlivares <efrain@rightscale.com>
2012-01-25 22:06:48 +0000

    Added hello_world cookbook

---
+++ cookbooks/hello_world/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: hello_world
+# Recipe:: default
+#
+# Copyright 2012, RightScale <efrain@rightscale.com> 
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World!"
